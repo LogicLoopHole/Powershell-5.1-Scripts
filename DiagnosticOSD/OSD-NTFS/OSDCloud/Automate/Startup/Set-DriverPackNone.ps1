25ba571c099880ee0ea1943adc29fc5bd2728a59
LogicLoopHole <207279606+LogicLoopHole@users.noreply.github.com>
2026-03-18 17:53:59 -0700

    Delete DiagnosticOSD directory
    prepping for generation 2 redesign of DiagOSD

--- DiagnosticOSD/OSD-NTFS/OSDCloud/Automate/Startup/Set-DriverPackNone.ps1
+++
@@ -1,7 +0,0 @@
-# Set-DriverPackNone.ps1
-# Runs inside Invoke-OSDCloud process via Automate\Startup
-# Prevents OSDCloud from downloading OEM DriverPacks
-$Global:OSDCloud.DriverPackName = 'None'
-Write-Host "  DriverPackName set to 'None' - using Custom\OfflineDrivers instead" -ForegroundColor Green
-
-pause

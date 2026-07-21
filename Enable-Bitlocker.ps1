5fd956c61ba6a291e5911cf8d59dd0861e39d99d
LogicLoopHole <207279606+LogicLoopHole@users.noreply.github.com>
2026-07-20 17:30:51 -0700

    Update Enable-Bitlocker.ps1

--- Enable-Bitlocker.ps1
+++ Enable-Bitlocker.ps1
@@ -1,3 +1,5 @@
 $v = gwmi -Namespace root\cimv2\Security\MicrosoftVolumeEncryption Win32_EncryptableVolume -Filter "DriveLetter='C:'"
+$v.ProtectKeyWithTPM($null,$null).ReturnValue
+$r=$v.ProtectKeyWithNumericalPassword($null,$null); $r.ReturnValue
+$v.BackupRecoveryInformationToActiveDirectory($r.VolumeKeyProtectorID).ReturnValue
 $v.Encrypt(0,1).ReturnValue
-$v.GetEncryptionMethod().EncryptionMethod
